722982f7cf6b2e7ee8ade9318239738ca7124f2c
Smooth <1057213071@qq.com>
2025-06-08 13:52:19 +0800

    ✨ feat: 添加分析模块，包括控制器、服务及相关DTO，整合至主应用模块。

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
 import { TrackwebModule } from './trackweb/trackweb.module';
 import { AuthModule } from './auth/auth.module';
+import { AnalyzeModule } from './analyze/analyze.module';
 
 @Module({
-  imports: [PrismaModule, TrackwebModule, AuthModule],
+  imports: [PrismaModule, TrackwebModule, AuthModule, AnalyzeModule],
 })
 export class AppModule {}
